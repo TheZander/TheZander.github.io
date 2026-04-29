07882e1154868b7ec91a96d28d4d809c62ae6317
TheZander <felix.meadows13@gmail.com>
2026-04-29 11:43:20 -0400

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,65 @@
+ideologies = [
+    {
+        "name": "Terra",
+        "stats": {
+            "econ": 70,
+            "dipl": 90,
+            "govt": 90,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Chil",
+        "stats": {
+            "econ": 90,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Moxie",
+        "stats": {
+            "econ": 70,
+            "dipl": 50,
+            "govt": 10,
+            "scty": 50
+        }
+    },
+    {
+        "name": "POND",
+        "stats": {
+            "econ": 70,
+            "dipl": 30,
+            "govt": 70,
+            "scty": 30
+        }
+    },
+    {
+        "name": "RA Party",
+        "stats": {
+            "econ": 50,
+            "dipl": 30,
+            "govt": 10,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Lake",
+        "stats": {
+            "econ": 30,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "POWS",
+        "stats": {
+            "econ": 30,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 50
+        }
+    }
+];
---
+++ questions.js
@@ -0,0 +1,101 @@
+questions = [
+    {
+        "question": "We should consider returning to Zenny-Piperism",
+        "effect": {
+            "econ": 3,
+            "dipl": 1,
+            "govt": 10,
+            "scty": 2
+        }
+    },
+    {
+        "question": "There are too many parties.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "We should leave NS-Left",
+        "effect": {
+            "econ": -1,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Socialism or Communism are benifital Ideoligies",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The Chair of Presidential Security should not exist as a role",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The Govnermnet is currently functional",
+        "effect": {
+            "econ": 0,
+            "dipl": -1,
+            "govt": 3,
+            "scty": -2
+        }
+    },
+    {
+        "question": "We need a new constitution",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "We need to focus internaly rather than externaly",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Capitalism is a supurior system",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "More Parties with more ideas will be good in the long run",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "We Should Establish a Military",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+];
